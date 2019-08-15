9227c3e964e6fd32b83c819d0ffc3f5503277fdb
Juan Gonzalez <juan.gonzalez@blackboard.com>
2019-08-15 14:14:19 -0500

    finished smart contract and tested all functionalities

--- contracts/Goryplace.sol
+++ contracts/Goryplace.sol
@@ -4,12 +4,12 @@ contract Goryplace {
   string public name;
   uint public productCount = 0;
   mapping(uint => Product) public products;
-  
+
   struct Product {
     uint id;
     string name;
     uint price;
-    address owner;
+    address payable owner;
     bool purchased;
   }
 
@@ -17,20 +17,45 @@ contract Goryplace {
     uint id,
     string name,
     uint price,
-    address owner,
+    address payable owner,
     bool purchased
   );
-
+  event ProductPurchased(
+    uint id,
+    string name,
+    uint price,
+    address payable owner,
+    bool purchased
+  );
   constructor() public {
     name = "la goryTienda";
   }
 
   function createProduct(string memory _name, uint _price) public {
+     // validate name was inputed
     require(bytes(_name).length> 0);
-    require(_price > 0);
+
+    // validate price
+    require(_price > 0, 'price must be gratter than 0');
     productCount++;
-    products[productCount] = Product(productCount, _name, _price, msg.sender, false );
+    products[productCount] = Product(productCount, _name, _price, msg.sender, false ); // create product
     emit ProductCreated(productCount, _name, _price, msg.sender, false );
   }
-  
+
+  function purchaseProduct(uint _id) public payable {
+    Product memory _product = products[_id]; //Fetch product
+    address payable _seller = _product.owner; // Fetch owner
+
+    require(_product.id > 0 && _product.id <= productCount, 'the product does not exists' );
+    require(msg.value >= _product.price, 'You dont have money');
+    require(!_product.purchased, 'The product has been sold');
+
+    _product.owner = msg.sender;
+    _product.purchased = true;
+    products[_id] = _product;
+    address(_seller).transfer(msg.value);
+    emit ProductPurchased(productCount, _product.name, _product.price, msg.sender, true );
+
+  }
+
 }
--- test/Goryplace.test.js
+++ test/Goryplace.test.js
@@ -3,7 +3,7 @@ require('chai')
   .use(require('chai-as-promised'))
   .should()
 contract('Goryplace', ([deployer, seller, buyer]) => {
-  let goryplace 
+  let goryplace
   before( async () => {
     goryplace = await Goryplace.deployed()
   })
@@ -20,7 +20,7 @@ contract('Goryplace', ([deployer, seller, buyer]) => {
       const name = await goryplace.name()
       assert.equal(name,'la goryTienda')
     })
-    
+
   })
 
   describe('Products', async () => {
@@ -31,6 +31,7 @@ contract('Goryplace', ([deployer, seller, buyer]) => {
     })
 
     it('should create a product', async () => {
+      //SUCCESS
       assert.equal(productCount, 1)
       const event = result.logs[0].args
       assert.equal(event.id.toNumber(), productCount, 'id is correct')
@@ -38,11 +39,55 @@ contract('Goryplace', ([deployer, seller, buyer]) => {
       assert.equal(event.price, '1000000000000000000', 'price is correct')
       assert.equal(event.owner, seller, 'owner is correct')
       assert.equal(event.purchased, false, 'purchased is correct')
+      //Failure
       await await goryplace.createProduct('', web3.utils.toWei('1', 'Ether'), {from: seller}).should.be.rejected;
       await await goryplace.createProduct('goryJugo', 0, {from: seller}).should.be.rejected;
     })
 
-    
+    it('should list products', async () => {
+      const product = await goryplace.products(productCount)
+      assert.equal(product.id.toNumber(), productCount, 'id is correct')
+      assert.equal(product.name, 'goryJugo', 'name is correct')
+      assert.equal(product.price, '1000000000000000000', 'price is correct')
+      assert.equal(product.owner, seller, 'owner is correct')
+      assert.equal(product.purchased, false, 'purchased is correct')
+    })
+
+    it('should sell product', async () => {
+      //Track seller balance
+      let oldSellerBalance
+      oldSellerBalance = await web3.eth.getBalance(seller)
+      oldSellerBalance = new web3.utils.BN(oldSellerBalance)
+
+      result = await goryplace.purchaseProduct(productCount, { from: buyer, value: web3.utils.toWei('1', 'Ether')})
+      const event = result.logs[0].args
+      assert.equal(event.id.toNumber(), productCount, 'id is correct')
+      assert.equal(event.name, 'goryJugo', 'name is correct')
+      assert.equal(event.price, '1000000000000000000', 'price is correct')
+      assert.equal(event.owner, buyer, 'owner is correct')
+      assert.equal(event.purchased, true, 'purchased is correct')
+
+      //Check seller receive the funds
+      let newSellerBalance
+      newSellerBalance = await web3.eth.getBalance(seller)
+      newSellerBalance = new web3.utils.BN(newSellerBalance)
+
+      let price
+      price = web3.utils.toWei('1', 'Ether')
+      price = new web3.utils.BN(price)
+
+      const expectedBalance = oldSellerBalance.add(price)
+      assert.equal(newSellerBalance.toString(), expectedBalance.toString())
+
+      //FAILURE: Buy product that does not exist
+      await goryplace.purchaseProduct(1111, { from: buyer, value: web3.utils.toWei('1', 'Ether')}).should.be.rejected
+      //FAILURE: Buy without enough money
+      await goryplace.purchaseProduct(productCount, { from: buyer, value: web3.utils.toWei('0.5', 'Ether')}).should.be.rejected
+      // FAILURE: Buy a product that is already been sold.
+      await goryplace.purchaseProduct(productCount, { from: deployer, value: web3.utils.toWei('1', 'Ether')}).should.be.rejected
+    })
+
+
   })
-  
+
 })
